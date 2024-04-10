5b33c9ca52bbec8a898700b6b1b53dbcd35913e6
anderenaduer90 <166590387+anderenaduer90@users.noreply.github.com>
2024-04-10 10:19:27 +0200

    Add files via upload

---
+++ js/submit.js
@@ -0,0 +1,9 @@
+
+
+  function zeigePopup() {
+    document.getElementById("popup-container").style.display = "block";
+  }
+
+  function schließePopup() {
+    document.getElementById("popup-container").style.display = "none";
+  }
